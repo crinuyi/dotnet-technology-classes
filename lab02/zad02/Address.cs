1d80ffcdd0808a83060618824cd249ac12eadda9
crin <crinuyi@gmail.com>
2018-10-29 16:02:46 +0100

    added Address.cs and Person.cs

---
+++ lab02/zad02/Address.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad02 {
+    struct Address {
+        private string streetName;
+        private string city;
+        private int houseNumber;
+        private int apartmentNumber;
+
+        public Address(string streetName, string city, int houseNumber, int apartmentNumber) {
+            this.streetName = streetName;
+            this.city = city;
+            this.houseNumber = houseNumber;
+            this.apartmentNumber = apartmentNumber;
+        }
+
+        public Address(int houseNumber, int apartmentNumber, string city)
+            : this("Aleje Jerozolimskie", city, houseNumber, apartmentNumber) { }
+
+        public Address(string streetName, int houseNumber, int apartmentNumber)
+            : this(streetName, "Warszawa", houseNumber, apartmentNumber) { }
+
+        public Address(int houseNumber, int apartmentNumber)
+            :this("Aleje Jerozolimskie", "Warszawa", houseNumber, apartmentNumber) { }
+    }
+}
