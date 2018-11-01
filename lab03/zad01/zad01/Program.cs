d37ac49467a24750cdf6dd935db0a6bbce6f4fa5
crin <crinuyi@gmail.com>
2018-11-01 17:04:20 +0100

    03/01

--- lab03/zad01/zad01/Program.cs
+++ lab03/zad01/zad01/Program.cs
@@ -7,7 +7,32 @@ using System.Threading.Tasks;
 namespace zad01 {
     class Program {
         static void Main(string[] args) {
+            UnitsOfMeasure.Quantity piece = new UnitsOfMeasure.Quantity();
+            piece.Piece = 50;
+            Console.WriteLine("** ILOŚĆ **\n" +
+                "   Sztuki (bazowa wartość): {0}\n" +
+                "   Kopa: {1}\n" +
+                "   Tuzin: {2}\n" +
+                "   Mendel: {3}\n\n",
+                piece.Piece, piece.getSixty(), piece.getDozen(), piece.getMendel());
 
+            UnitsOfMeasure.Length meter = new UnitsOfMeasure.Length();
+            meter.Meter = 10;
+            Console.WriteLine("** DŁUGOŚĆ **\n" +
+                "   Metr (bazowa wartość): {0}\n" +
+                "   Milimetr: {1}\n" +
+                "   Cal: {2}\n" +
+                "   Yard: {3}\n" +
+                "   Mila morska: {4}\n" +
+                "   Mila angielska: {5}\n\n",
+                meter.Meter, meter.getMilimeter(), meter.getInch(), meter.getYard(), meter.getNauticalMile(), meter.getEnglishMile());
+
+            UnitsOfMeasure.Mass kilogram = new UnitsOfMeasure.Mass();
+            kilogram.Kilogram = 35;
+            Console.WriteLine("** MASA **\n" +
+                "   Kilogram (bazowa wartość): {0}\n" +
+                "   Gram: {1}",
+                kilogram.Kilogram, kilogram.getGram());
         }
     }
 }
--- lab03/zad01/zad01/Quantity.cs
+++ lab03/zad01/zad01/Quantity.cs
@@ -7,49 +7,61 @@ using System.Threading.Tasks;
 namespace zad01 {
     public class UnitsOfMeasure {
         public class Quantity {
-            private int piece { get; set; }
-
-            public Quantity(int piece) {
-                this.piece = piece;
+            private int piece;
+            public int Piece {
+                get { return piece; }
+                set { this.piece = value; }
             }
 
-            public int getSixty(int piece) {  //kopa
+            public int getSixty() {  //kopa
                 return piece * 60;
             }
 
-            public int getDozen(int piece) {  //tuzin
+            public int getDozen() {  //tuzin
                 return piece * 12;
             }
 
-            public int getMendel(int piece) { //mendel
+            public int getMendel() { //mendel
                 return piece * 15;
             }
         }
 
         public class Length {
-            public double getMilimeter(double meter) {      //milimetr
+            private double meter;
+            public double Meter {
+                set { this.meter = value; }
+                get { return meter; }
+            }
+
+            public double getMilimeter() {      //milimetr
                 return meter * 0.001;
             }
 
-            public double getInch(double meter) {           //cal
+            public double getInch() {           //cal
                 return meter * 25.4;
             }
 
-            public double getYard(double meter) {           //yard
+            public double getYard() {           //yard
                 return meter * 0.9144;
             }
 
-            public double getNauticalMile(double meter) {   //mila morska
+            public double getNauticalMile() {   //mila morska
                 return meter * 1852;
             }
 
-            public double getEnglishMile(double meter) {    //mila angielska
+            public double getEnglishMile() {    //mila angielska
                 return meter * 1609.344;
             }
         }
 
         public class Mass {
-            public double getGram(double kilogram) {    //gram
+            private double kilogram;
+            public double Kilogram {
+                get { return kilogram; }
+                set { this.kilogram = value; }
+            }
+
+            public double getGram() {    //gram
                 return kilogram * 0.001;
             }
         }
