e4b9b4c2af0262dbf1f032c163e9fef796a60fd1
crin <crinuyi@gmail.com>
2018-11-01 22:01:49 +0100

    03/02

---
+++ lab03/zad02/NumberOfDays.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad02 {
+    public class NumberOfDays {
+        private string[] months = {
+            "styczeń",
+            "luty",
+            "marzec",
+            "kwiecień",
+            "maj",
+            "czerwiec",
+            "lipiec",
+            "sierpień",
+            "wrzesień",
+            "październik",
+            "listopad",
+            "grudzień"
+        };
+        private int[] days = {
+            31,
+            28,
+            31,
+            30,
+            31,
+            30,
+            31,
+            31,
+            30,
+            31,
+            30,
+            31
+        };
+
+        private int FindNumberOfDays(string month) {
+            for(int i=0; i<months.Length; i++) {
+                if (month.ToLower().Equals(months[i]))
+                    return days[i];
+            }
+            return 1;
+        }
+
+        //indeksator
+        public int this[string month] {
+            get => FindNumberOfDays(month);
+        }
+    }
+}
