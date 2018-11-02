2da8b8cc9c62eb2073e2d4e2358ed1338768026e
crin <crinuyi@gmail.com>
2018-11-02 13:01:10 +0100

    04/01

---
+++ lab04/zad01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad01 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(5.factorial());
+            Console.WriteLine(10.binomial(5));
+        }
+    }
+}
---
+++ lab04/zad01/intExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad01 {
+    static class IntExtension {
+        public static int factorial(this int x) {
+            if (x == 0)
+                return 1;
+            else
+                return x * factorial(x - 1);
+        }
+
+        public static int binomial(this int x, int y) {
+            if ((x == y) || (y == 0))
+                return 1;
+            else
+                return binomial(x - 1, y) + binomial(x - 1, y - 1);
+        }
+    }
+}
