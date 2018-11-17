87a7dec81df43dd56da535cfa8391011a0ed579e
crin <crinuyi@gmail.com>
2018-11-17 13:41:41 +0100

    08/01

--- lab08/zad01/Car.cs
+++ lab08/zad01/Car.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace zad01 {
-    class Car {
+    public class Car {
         internal string brand;
         internal string model;
         internal int yearOfProduction;
@@ -18,15 +18,61 @@ namespace zad01 {
             this.yearOfProduction = yearOfProduction;
             this.price = price;
         }
+
+        public static List<Car> AddCars() {
+            List<Car> cars = new List<Car>();
+            Car car1 = new Car("Audi", "A8", 1994, 56424);
+            Car car2 = new Car("Ford", "GT", 2016, 78671);
+            Car car3 = new Car("Porsche", "911", 2005, 76545);
+            Car car4 = new Car("Ford", "S-Max", 2015, 76432);
+            Car car5 = new Car("Audi", "TT", 1998, 65443);
+            Car car6 = new Car("BMW", "i3", 2013, 65874);
+            Car car7 = new Car("BMW", "X6", 2008, 79851);
+            Car car8 = new Car("Ford", "Mustang", 2014, 98763);
+            Car car9 = new Car("Porsche", "911", 2003, 78655);
+            Car car10 = new Car("Audi", "A2", 2000, 54367);
+            cars.Add(car1);
+            cars.Add(car2);
+            cars.Add(car3);
+            cars.Add(car4);
+            cars.Add(car5);
+            cars.Add(car6);
+            cars.Add(car7);
+            cars.Add(car8);
+            cars.Add(car8);
+            cars.Add(car9);
+            cars.Add(car10);
+            return cars;
+        }
     }
 
-    public class CompareByYear : IComparer {
-        public int Compare(object a, object b) {
-            Car c1 = (Car)a;
-            Car c2 = (Car)b;
-            if (c1.yearOfProduction > c2.yearOfProduction)
+    public class CompareByYear : IComparer<Car> {
+        public int Compare(Car a, Car b) {
+            if (a.yearOfProduction > b.yearOfProduction)
                 return 1;
-            if (c2.yearOfProduction > c1.yearOfProduction)
+            if (b.yearOfProduction > a.yearOfProduction)
+                return -1;
+            else
+                return 0;
+        }
+    }
+
+    public class CompareByPrice : IComparer<Car> {
+        public int Compare(Car a, Car b) {
+            if (a.price > b.price)
+                return 1;
+            if (b.price > a.price)
+                return -1;
+            else
+                return 0;
+        }
+    }
+
+    public class CompareByBrand : IComparer<Car> {
+        public int Compare(Car a, Car b) {
+            /*if (a.brand > b.brand)
+                return 1;
+            */if (b.price > a.price)
                 return -1;
             else
                 return 0;
--- lab08/zad01/Program.cs
+++ lab08/zad01/Program.cs
@@ -7,50 +7,80 @@ using System.Threading.Tasks;
 namespace zad01 {
     class Program {
         static void Main(string[] args) {
-            int intTemp = -1;
+            int intTemp;
+            CompareByYear compareByYear = new CompareByYear();
+            CompareByPrice compareByPrice = new CompareByPrice();
 
             //dodanie samochodów do programu
-            Car car1 = new Car("Audi", "A8", 1994, 56424);
-            Car car2 = new Car("Ford", "GT", 2016, 78671);
-            Car car3 = new Car("Porsche", "911", 2005, 76545);
-            Car car4 = new Car("Ford", "S-Max", 2015, 76432);
-            Car car5 = new Car("Audi", "TT", 1998, 65443);
-            Car car6 = new Car("BMW", "i3", 2013, 65874);
-            Car car7 = new Car("BMW", "X6", 2008, 79851);
-            Car car8 = new Car("Ford", "Mustang", 2014, 98763);
-            Car car9 = new Car("Porsche", "911", 2003, 78655);
-            Car car10 = new Car("Audi", "A2", 2000, 54367);
+            List<Car> cars = Car.AddCars();
+            Console.WriteLine(
+                "\n\n" +
+                "\tWybierz po czym chcesz sortować samochody:\n" +
+                "\t 1: marka\n" +
+                "\t 2: model\n" +
+                "\t 3: rok produkcji\n" +
+                "\t 4: cena\n" +
+                "\tAby wyświetlić nieposortowane auta wpisz 5.\n");
+            intTemp = Convert.ToInt32(Console.ReadLine());
 
-            do {
-                Console.WriteLine(
-                    "\tWitaj w programie zawierającym listę samochodów dostępnych w komisie.\n" +
-                    "\tWybierz po czym chcesz sortować samochody:\n" +
-                    "\t 1: marka\n" +
-                    "\t 2: model\n" +
-                    "\t 3: rok produkcji\n" +
-                    "\t 4: cena\n" +
-                    "\tAby zamknąć program wpisz 0.");
-                intTemp = Convert.ToInt32(Console.ReadLine());
-
-                switch (intTemp) {
-                    case 1: {
-                            break;
-                        }
-                    case 2: {
-                            break;
-                        }
-                    case 3: {
-                            
-                            break;
-                        }
-                    case 4: {
-                            break;
-                        }
-                    default:
-                        Console.WriteLine("\tWpisałeś niepoprawną wartość!\n");
+            switch (intTemp) {
+                case 1: {
+                        //Użycie LINQ
+                        var sortedCars = cars.OrderBy(x => x.brand).ToList();
+                        foreach (Car car in sortedCars)
+                            Console.WriteLine(
+                                car.brand + ", " +
+                                car.model + ", " +
+                                car.yearOfProduction + ", " +
+                                car.price + " zł");
                         break;
-                }
-            } while (intTemp != 0);
+                    }
+                case 2: {
+                        //Użycie LINQ
+                        var sortedCars = cars.OrderBy(x => x.model);
+                        foreach (Car car in sortedCars)
+                            Console.WriteLine(
+                                car.brand + ", " +
+                                car.model + ", " +
+                                car.yearOfProduction + ", " +
+                                car.price + " zł");
+                        break;
+                    }
+                case 3: {
+                        //użycie IComparer
+                        cars.Sort(compareByYear);
+                        foreach (Car car in cars)
+                            Console.WriteLine(
+                                car.brand + ", " +
+                                car.model + ", " +
+                                car.yearOfProduction + ", " +
+                                car.price + " zł");
+                        break;
+                    }
+                case 4: {
+                        //użycie IComparer
+                        cars.Sort(compareByPrice);
+                        foreach (Car car in cars)
+                            Console.WriteLine(
+                                car.brand + ", " +
+                                car.model + ", " +
+                                car.yearOfProduction + ", " +
+                                car.price + " zł");
+                        break;
+                    }
+                case 5: {
+                        foreach (Car car in cars)
+                            Console.WriteLine(
+                                car.brand + ", " +
+                                car.model + ", " +
+                                car.yearOfProduction + ", " +
+                                car.price + " zł");
+                        break;
+                    }
+                default:
+                    Console.WriteLine("\tWpisałeś niepoprawną wartość!\n");
+                    break;
+            }
         }
     }
 }
